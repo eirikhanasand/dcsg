9a6c2ce1352f436a1d25029199da42248a9bd167
eirikhanasand <eirik.hanasand@gmail.com>
2024-01-29 23:00:42 +0100

    Removed missing servers

--- src/content/log.ts
+++ src/content/log.ts
@@ -4,8 +4,9 @@ export const servers = [
     { name: 'ww1', state: 0 }, 
     { name: 'ww2', state: 0 }, 
     { name: 'ww3', state: 0 }, 
-    { name: 'ww4', state: 0 }, 
-    { name: 'balancer', state: 0 }
+    { name: 'balancer', state: 0 },
+    { name: 'balancer2', state: 0 },
+    { name: 'balancer3', state: 0 }
 ]
 
 export const services = [
