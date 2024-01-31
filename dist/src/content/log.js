bcf0c97f130141da2adec9c126a7dbc297a3febe
eirikhanasand <eirik.hanasand@gmail.com>
2024-01-31 05:06:03 +0100

    Compiled changes

--- dist/src/content/log.js
+++ dist/src/content/log.js
@@ -1,11 +1,12 @@
 export const servers = [
     { name: 'manager', state: 0 },
-    { name: 'db1', state: 0 },
-    { name: 'ww1', state: 0 },
-    { name: 'ww2', state: 0 },
+    { name: 'balancer', state: 0 },
+    { name: 'db', state: 0 },
+    { name: 'www', state: 0 },
     { name: 'ww3', state: 0 },
-    { name: 'ww4', state: 0 },
-    { name: 'balancer', state: 0 }
+    { name: 'worker', state: 0 },
+    { name: 'worker2', state: 0 },
+    { name: 'worker3', state: 0 }
 ];
 export const services = [
     { host: "manager", service: "uc status", state: '' },
