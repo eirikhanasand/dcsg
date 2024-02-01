937f1c27bfb4b74ce3d1503e54297ffcf3a1be3a
eirikhanasand <eirik.hanasand@gmail.com>
2024-02-01 03:10:30 +0100

    Updated order of importance

--- src/content/log.ts
+++ src/content/log.ts
@@ -1,9 +1,9 @@
 export const servers = [
-    { name: 'manager', state: 0 }, 
-    { name: 'balancer', state: 0 }, 
-    { name: 'varnish', state: 0 }, 
-    { name: 'www', state: 0 }, 
-    { name: 'db', state: 0 }, 
+    { name: 'manager', state: 0 },
+    { name: 'balancer', state: 0 },
+    { name: 'varnish', state: 0 },
+    { name: 'db', state: 0 },
+    { name: 'www', state: 0 },
     { name: 'worker', state: 0 },
     { name: 'worker2', state: 0 },
     { name: 'worker3', state: 0 }
