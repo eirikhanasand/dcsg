e09b453ae03ec056c0fd4ad071555d28ac977411
eirikhanasand <eirik.hanasand@gmail.com>
2024-02-13 12:18:38 +0100

    Updated server list

--- dist/src/content/log.js
+++ dist/src/content/log.js
@@ -4,7 +4,6 @@ export const servers = [
     { name: 'balancer', ip: '192.168.129.18', state: 0 },
     { name: 'memcache', ip: '192.168.133.38', state: 0 },
     { name: 'db', ip: '192.168.131.219', state: 0 },
-    { name: 'www', ip: '192.168.131.245', state: 0 },
     { name: 'worker', ip: '192.168.132.22', state: 0 },
     { name: 'worker2', ip: '192.168.131.200', state: 0 },
 ];
