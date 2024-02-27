96ac3ff2472ea9b29e5514e75f262cfc883d00c0
eirikhanasand <eirik.hanasand@gmail.com>
2024-02-27 13:37:36 +0100

    Added worker2

--- dist/src/content/log.js
+++ dist/src/content/log.js
@@ -2,6 +2,7 @@ export const servers = [
     { name: 'manager', ip: '192.168.130.128', state: 0 },
     { name: 'varnish', ip: '192.168.132.227', state: 0 },
     { name: 'memcache', ip: '192.168.133.38', state: 0 },
+    { name: 'worker2', ip: '192.168.131.200', state: 0 },
     { name: 'db', ip: '192.168.131.219', state: 0 },
     { name: 'backup', ip: '192.168.128.228', state: 0 },
     { name: 'server1', ip: '192.168.129.172', state: 0 },
