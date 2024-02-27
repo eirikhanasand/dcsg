5d5326da9244a5f5fdb39392c51ad7100e1f1797
eirikhanasand <eirik.hanasand@gmail.com>
2024-02-27 12:37:09 +0100

    Removed worker2

--- dist/src/content/log.js
+++ dist/src/content/log.js
@@ -3,7 +3,6 @@ export const servers = [
     { name: 'varnish', ip: '192.168.132.227', state: 0 },
     { name: 'memcache', ip: '192.168.133.38', state: 0 },
     { name: 'db', ip: '192.168.131.219', state: 0 },
-    { name: 'worker2', ip: '192.168.131.200', state: 0 },
     { name: 'backup', ip: '192.168.128.228', state: 0 },
     { name: 'server1', ip: '192.168.129.172', state: 0 },
     { name: 'server2', ip: '192.168.132.103', state: 0 },
